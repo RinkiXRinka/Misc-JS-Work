31af53516e5e14e06197576ed2db083db746e475
Miguel Cabrera <59181765+cabreram-dev@users.noreply.github.com>
2022-02-27 22:24:40 -0600

    Create testingBooleans.js

---
+++ testingBooleans.js
@@ -0,0 +1,14 @@
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+
+ console.log(Boolean(c > d));
+ console.log(Boolean('Name' === 'Names'));
+ console.log(Boolean (a < b < c));
+
+/*
+true
+false
+true
+/*
