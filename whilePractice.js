fae37b915088512fe05493a14f08eb4f75ad38dc
Miguel Cabrera <59181765+cabreram-dev@users.noreply.github.com>
2022-03-02 20:23:07 -0600

    Create whilePractice.js

---
+++ whilePractice.js
@@ -0,0 +1,13 @@
+let letters = "A";
+let i = 0;
+
+//Will start a while loop that will run as long as i is lesser than 20
+while (i < 20) {
+	// Will Concatenate an aditional 'A' to the variable letters
+	letters += "A";
+	//Increments the value of i by 1
+	i++;
+}
+//outputs the variable letters in the console.
+console.log(letters);
+
