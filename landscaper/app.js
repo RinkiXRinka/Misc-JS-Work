ab37addec715ccbff5f52232955c4c11bd2f1931
Miguel Cabrera <59181765+cabreram-dev@users.noreply.github.com>
2022-06-07 15:06:05 -0500

    Added reset button

--- landscaper/app.js
+++ landscaper/app.js
@@ -2,6 +2,7 @@ let money = 0;
 let inventory = ["Teeth"];
 let add_money;
 let add_inventory;
+let reset_money = 0;
 
 function starting(){
 
@@ -9,7 +10,6 @@ add_money = document.getElementById("money");
 add_inventory = document.getElementById("inventory");
 
 add_money.innerHTML = money;
-
 add_inventory.innerHTML = inventory;
 
 document.getElementById("buyScissors").disabled = true;
@@ -49,26 +49,48 @@ function cutWithLawnmower(){
 function shop(){
     if(money >= 5){
         document.getElementById("cutWithScissors").style.display = "inline"
-        document.getElementById("buyScissors").disabled = true
-        if (inventory[1]== "Scissors"){
-            console.log("no hacer nada")
+        document.getElementById("buyScissors").style.display = "none"
+        if (inventory[1] != "Scissors"){
+            inventory.push("Scissors")
         }
-        else inventory.push("Scissors")
+
         add_inventory = document.getElementById("inventory");
         add_inventory.innerHTML = inventory;
 
     }
     if(money >= 25){
         document.getElementById("cutWithLawnmower").style.display = "inline"
-        document.getElementById("buyLawnmower").disabled = true
-        if (inventory[2]== "Lawnmower"){
-            console.log("no hacer nada")
+        document.getElementById("buyLawnmower").style.display = "none"
+        if (inventory[2] != "Lawnmower"){
+            inventory.push("Lawnmower")
         }
-        else inventory.push("Lawnmower")
+
         add_inventory = document.getElementById("inventory");
         add_inventory.innerHTML = inventory;
-        
 
     }
 
+}
+function reset(){
+    money = 0;
+    add_money.value = money;
+    add_money.innerHTML = add_money.value;
+    document.getElementById("buyScissors").disabled = true;
+    document.getElementById("buyLawnmower").disabled = true;
+    document.getElementById("cutWithScissors").style.display = "none"
+    document.getElementById("cutWithLawnmower").style.display = "none"
+
+    inventory.length = 1
+    add_inventory.innerHTML = inventory;
+
+    if(document.getElementById("buyScissors").style.display != "inline"){
+
+        document.getElementById("buyScissors").style.display = "inline"
+        }
+    
+    if(document.getElementById("buyLawnmower").style.display != "inline"){
+
+        document.getElementById("buyLawnmower").style.display = "inline"
+        }
+    
 }
